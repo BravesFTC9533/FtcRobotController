fd8f0f6e8d9d64a794b4dc4c6b7c42c14619a072
Nick <nickfanelli5@gmail.com>
2020-10-05 21:35:12 -0400

    Added in the basic common and drive functionallity needed to create a robot and mecanum drive system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Robot.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+
+    public static final double WHEEL_DISTANCE_INCHES = 18.1;
+
+    public static final double            NEVE_COUNTS_PER_MOTOR_REV = 1120;      // eg: NeveRest Side motor
+    public static final double            DRIVE_GEAR_REDUCTION    = 45.0 / 35.0;             // This is < 1.0 if geared UP
+    public static final double            WHEEL_DIAMETER_INCHES   = 5.250;           // For figuring circumference
+    public static final double     COUNTS_PER_INCH = (NEVE_COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    public final DcMotorEx frontLeft;
+    public final DcMotorEx frontRight;
+    public final DcMotorEx backLeft;
+    public final DcMotorEx backRight;
+
+    public final DcMotorEx[] allMotors;
+    public final DcMotorEx[] leftMotors;
+    public final DcMotorEx[] rightMotors;
+
+    private boolean isReversed = false;
+
+    public Robot(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "front_left");
+        frontRight = hardwareMap.get(DcMotorEx.class, "front_right");
+        backLeft = hardwareMap.get(DcMotorEx.class, "back_left");
+        backRight = hardwareMap.get(DcMotorEx.class, "back_right");
+
+        this.allMotors = new DcMotorEx[] { frontLeft, frontRight, backLeft, backRight };
+        this.rightMotors = new DcMotorEx[] { frontRight, backRight };
+        this.leftMotors = new DcMotorEx[] { frontLeft, backLeft };
+
+        // TODO: Reverse two motors
+
+        this.frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        this.frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        this.backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        this.backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        this.setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setMotorMode(DcMotor.RunMode runMode) {
+        this.frontLeft.setMode(runMode);
+        this.frontRight.setMode(runMode);
+        this.backLeft.setMode(runMode);
+        this.backRight.setMode(runMode);
+    }
+
+    public boolean isBusy() { return frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy(); }
+
+    public void setIsReverse(boolean isReverse) {
+        this.isReversed = isReverse;
+
+        for(DcMotorEx motor : allMotors) { motor.setDirection(motor.getDirection().inverted()); }
+    }
+
+    public boolean isReverse() { return isReversed; }
+
+    public void stop() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
