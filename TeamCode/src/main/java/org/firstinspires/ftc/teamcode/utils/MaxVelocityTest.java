cc0520c877ef31cc1b69af46267c1a2fdf8bf53f
Nick <nickfanelli5@gmail.com>
2020-11-15 11:50:32 -0500

    Filled in the max velocity class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MaxVelocityTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MaxVelocityTest.java
@@ -1,4 +1,55 @@
 package org.firstinspires.ftc.teamcode.utils;
 
-public class MaxVelocityTest {
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="Max Velocity", group="Util Opmode")
+public class MaxVelocityTest extends LinearOpMode {
+
+    DcMotorEx motor;
+    double currentVelocity;
+    double maxVelocity = 0.0;
+
+    @Override
+
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        motor = hardwareMap.get(DcMotorEx.class, "front_left");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        telemetry.addLine("Motor labeled fl on start will rotate at max power.");
+        telemetry.addLine("Disclaimer: Make sure that the battery is charged");
+        telemetry.update();
+
+        waitForStart();
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+
+        motor.setPower(1);
+
+        while (opModeIsActive()) {
+
+            currentVelocity = motor.getVelocity();
+
+            if (currentVelocity > maxVelocity) {
+                maxVelocity = currentVelocity;
+            }
+
+            telemetry.addData("Current Velocity", currentVelocity);
+            telemetry.addData("Maximum Velocity", maxVelocity);
+            telemetry.update();
+
+        }
+
+    }
+
 }
